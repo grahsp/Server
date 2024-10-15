1909811af1d83c9525268816503a62b637f17f0c
grahsp <pontus.grahsp@gmail.com>
2024-10-15 20:15:33 +0200

    Add event handlers and properties to Client class

---
+++ Server/Client.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Sockets;
+
+namespace Server
+{
+    public class Client
+    {
+        // Events
+        public EventHandler? OnConnect;
+        public EventHandler? OnDisconnect;
+        public EventHandler<string>? OnDataReceived;
+
+        public string ID { get; }
+        public DateTime LastActive { get; private set; }
+
+        public NetworkStream Stream;
+        public bool IsConnected { get => _client.Connected; }
+
+        private TcpClient _client;
+        private CancellationTokenSource _cts = new();
+
+
+        public Client(TcpClient client)
+        {
+            _client = client;
+        }
+    }
+}
