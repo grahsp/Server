98deb8eca9f51796dc7f6f5ae39b719bc929851f
grahsp <pontus.grahsp@gmail.com>
2024-10-16 12:26:59 +0200

    Add NetworkTransceiver.Tests and library Moq for testing

---
+++ NetworkTransceiver.Tests/NetworkTransceiverTests.cs
@@ -0,0 +1,13 @@
+using Moq;
+
+namespace NetworkTransceiver.Tests
+{
+    [TestClass]
+    public class NetworkTransceiverTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
